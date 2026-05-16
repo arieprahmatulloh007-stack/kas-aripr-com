0deddc1a2d040d07e7d88d348f525bf5e6dd76ef
PutijaBandung <arieprahmatulloh007@gmail.com>
2026-05-16 20:11:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+function showPage(id){
+  const pages = document.querySelectorAll('.page');
 
+  pages.forEach(p=>{
+    p.style.display='none';
+  });
+
+  document.getElementById(id).style.display='block';
+}
+
+showPage('dashboard');
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Kas Masuk', 'Kas Keluar'],
+    datasets: [{
+      label: 'Statistik',
+      data: [5000000, 2000000]
+    }]
+  }
+});
