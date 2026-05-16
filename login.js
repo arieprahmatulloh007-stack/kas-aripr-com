597031f18feec6085efd3876ed40b7d728c60be6
PutijaBandung <arieprahmatulloh007@gmail.com>
2026-05-16 20:22:23 +0700

    Implement login function for user authentication

---
+++ login.js
@@ -0,0 +1,17 @@
+function login(){
+
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  if(username == 'admin' && password == 'admin123'){
+
+      localStorage.setItem('login','true');
+
+      window.location='index.html';
+
+  }else{
+
+      alert('Username atau password salah');
+
+  }
+}
